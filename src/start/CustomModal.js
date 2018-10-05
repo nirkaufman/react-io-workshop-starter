93c1674c7acacd5dc924c658e2aa67d795b9061f
nirkaufman <nirkaufman@gmail.com>
2018-10-05 15:42:42 -0400

    modal

---
+++ src/start/CustomModal.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class CustomModal extends Component {
+  render() {
+    return (
+      <div className="modal" style={modalStyle}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button className="close"><span>&times;</span></button>
+            </div>
+            <div className="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-secondary">Close</button>
+              <button className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const modalStyle = {
+  display: 'block',
+};
+
+export default CustomModal;
---
+++ src/start/RenderPropPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import CustomModal from "./CustomModal";
+
+class RenderPropPage extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Custom Modal</h2>
+        <CustomModal/>
+      </div>
+    );
+  }
+}
+
+export default RenderPropPage;
